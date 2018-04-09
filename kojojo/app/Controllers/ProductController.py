f75d5182b8bdc54a66058ee2cb404c163cb881a6
filedesless <filedesless@gmail.com>
2018-04-08 21:27:27 -0400

    added product deletion

--- kojojo/app/Controllers/ProductController.py
+++ kojojo/app/Controllers/ProductController.py
@@ -59,14 +59,20 @@ def list_products():
 @app.route('/Product/<int:id>', methods=['GET'])
 def show_product(id):
     with db.cursor() as cursor:
-        cursor.execute("SELECT ProductName, Price, Description, Date, TownName, Type, UserName, Email, Phone FROM Product "
+        cursor.execute("SELECT ProductId, ProductName, Price, Description, Date, TownName, Type, UserName, Email, Phone FROM Product "
                        "INNER JOIN User ON Product.UserId = User.UserId "
                        "INNER JOIN Town ON Product.TownId = Town.TownId "
                        "INNER JOIN Category ON Product.CategoryId = Category.CategoryId "
                        "WHERE ProductId = %s", (id,))
         product = cursor.fetchone()
+        if product is not None:
+            user = get_user()
+            is_author = user is not None and user["UserName"] == product["UserName"]
+        else:
+            product = []
+            is_author = False
 
-    return render_template('show_product.html', title='Accueil', product=product)
+    return render_template('show_product.html', title='Accueil', product=product, is_author=is_author)
 
 @app.route('/Product/Edit/<int:id>', methods=['GET'])
 def edit_product(id):
@@ -74,5 +80,19 @@ def edit_product(id):
 
 @app.route('/Product/Remove/<int:id>', methods=['GET'])
 def remove_product(id):
-    pass
+    user = get_user()
+    with db.cursor() as cursor:
+        cursor.execute("SELECT UserName FROM Product "
+                       "INNER JOIN User ON Product.UserId = User.UserId "
+                       "WHERE ProductId = %s", (id,))
+        product = cursor.fetchone()
+        if product is not None:
+            user = get_user()
+            if user is not None and user["UserName"] == product["UserName"]:
+                cursor.execute("DELETE FROM Product "
+                               "WHERE ProductId = %s", (id,))
+                db.commit()
+                flash("L'annonce a bien été supprimée")
+
+    return redirect('/')
 
