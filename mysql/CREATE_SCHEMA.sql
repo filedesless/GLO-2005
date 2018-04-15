9325245d6d699b7cad941a3edd730844b07f60c2
filedesless <filedesless@gmail.com>
2018-04-15 19:20:21 -0400

    fixed seeding

--- mysql/CREATE_SCHEMA.sql
+++ mysql/CREATE_SCHEMA.sql
@@ -1,4 +1,9 @@
 -- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
 -- -----------------------------------------------------
 -- Schema KOJOJO
 -- -----------------------------------------------------
@@ -41,8 +46,9 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `KOJOJO`.`Town` (
   `TownId` INT NOT NULL AUTO_INCREMENT,
-  `TownName` VARCHAR(45) NULL,
-  PRIMARY KEY (`TownId`))
+  `TownName` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`TownId`),
+  UNIQUE INDEX `TownName_UNIQUE` (`TownName` ASC))
 ENGINE = InnoDB;
 
 
@@ -112,4 +118,9 @@ CREATE TABLE IF NOT EXISTS `KOJOJO`.`Session` (
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
